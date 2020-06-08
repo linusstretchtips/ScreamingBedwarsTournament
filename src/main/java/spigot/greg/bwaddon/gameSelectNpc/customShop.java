988560c9f17dea4fb6bc229bcbd64f3fba3f4e37
Misat11 <misa.turek1@gmail.com>
2020-06-08 12:22:39 +0200

    move all things from ShopTransactionEvent to PostActionEvent

--- src/main/java/spigot/greg/bwaddon/gameSelectNpc/customShop.java
+++ src/main/java/spigot/greg/bwaddon/gameSelectNpc/customShop.java
@@ -50,15 +50,9 @@ public class customShop implements CommandExecutor, Listener {
         options.setRows(3);
         options.setRender_actual_rows(3);
         options.setRender_offset(0);
-        options.setRender_header_start(0);
-        options.setRender_footer_start(45);
-        options.setItems_on_row(9);
         options.setShowPageNumber(false);
-        options.setInventoryType(InventoryType.valueOf("CHEST"));
 
         options.setPrefix("§cBed§fwars §eTournament");
-        options.setGenericShop(true);
-        options.setGenericShopPriceTypeRequired(true);
         options.setAnimationsEnabled(true);
 
 
@@ -74,27 +68,6 @@ public class customShop implements CommandExecutor, Listener {
     }
 
     @EventHandler
-    public void onGeneratingItem(GenerateItemEvent event) {
-        if (event.getFormat() != format) {
-            return; // you should check if the format is yours
-        }
-
-        // here do some stuff on generating item with creator
-    }
-
-    @EventHandler
-    public void onPreAction(PreActionEvent event) {
-        if (event.getFormat() != format) {
-            return; // you should check if the format is yours
-        }
-
-        if (event.isCancelled()) {
-            return; // you should stop working when event is cancelled
-        }
-
-    }
-
-    @EventHandler
     public void onPostAction(PostActionEvent event) {
         if (event.getFormat() != format) {
             return; // you should check if the format is yours
@@ -104,8 +77,31 @@ public class customShop implements CommandExecutor, Listener {
             return; // you should stop working when event is cancelled
         }
 
-        // here you should make do some stuff when player clicks to item
-
+        if (event.getItem().getStack().getItemMeta().getDisplayName().contains("Game E1") && event.getPlayer().hasPermission("tournament.g1team1to4"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey");
+        if (event.getItem().getStack().getItemMeta().getDisplayName().contains("Game E2") && event.getPlayer().hasPermission("tournament.g1team5to8"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey2");
+        if (event.getItem().getStack().getItemMeta().getDisplayName().contains("Game E3") && event.getPlayer().hasPermission("tournament.g1team9to12"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey3");
+        if (event.getItem().getStack().getItemMeta().getDisplayName().contains("Game E4") && event.getPlayer().hasPermission("tournament.g1team13to16"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey4");
+        if (event.getItem().getStack().getItemMeta().getDisplayName().contains("Game E5") && event.getPlayer().hasPermission("tournament.g1team17to20"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey5");
+        if (event.getItem().getStack().getItemMeta().getDisplayName().contains("Game E6") && event.getPlayer().hasPermission("tournament.g1team21to24"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey6");
+        if (event.getItem().getStack().getItemMeta().getDisplayName().contains("Game E7") && event.getPlayer().hasPermission("tournament.g1team25to28"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey7");
+        if (event.getItem().getStack().getItemMeta().getDisplayName().contains("Game E8") && event.getPlayer().hasPermission("tournament.g1team29to32"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey8");
+        else if (event.getItem().getStack().getItemMeta().getDisplayName().contains("Game F1") && event.getPlayer().hasPermission("tournament.g2team1to2"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Village");
+        if (event.getItem().getStack().getItemMeta().getDisplayName().contains("Game F2") && event.getPlayer().hasPermission("tournament.g2team3to4"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Village2");
+        else if (event.getItem().getStack().getItemMeta().getDisplayName().contains("FINALS") && event.getPlayer().hasPermission("tournament.game3"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Deserted");
+        else {
+            event.getPlayer().sendMessage("§c&LHEY!, §cSorry but you Havnt got Permission to do this Yet!");
+        }
 
     }
 
@@ -120,35 +116,5 @@ public class customShop implements CommandExecutor, Listener {
         }
 
         return true;
-    }
-
-    @EventHandler
-    public void onShopTransaction(ShopTransactionEvent event) {
-
-        if (event.getStack().getItemMeta().getDisplayName().contains("Game E1") && event.getPlayer().hasPermission("tournament.g1team1to4"))
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey");
-        if (event.getStack().getItemMeta().getDisplayName().contains("Game E2") && event.getPlayer().hasPermission("tournament.g1team5to8"))
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey2");
-        if (event.getStack().getItemMeta().getDisplayName().contains("Game E3") && event.getPlayer().hasPermission("tournament.g1team9to12"))
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey3");
-        if (event.getStack().getItemMeta().getDisplayName().contains("Game E4") && event.getPlayer().hasPermission("tournament.g1team13to16"))
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey4");
-        if (event.getStack().getItemMeta().getDisplayName().contains("Game E5") && event.getPlayer().hasPermission("tournament.g1team17to20"))
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey5");
-        if (event.getStack().getItemMeta().getDisplayName().contains("Game E6") && event.getPlayer().hasPermission("tournament.g1team21to24"))
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey6");
-        if (event.getStack().getItemMeta().getDisplayName().contains("Game E7") && event.getPlayer().hasPermission("tournament.g1team25to28"))
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey7");
-        if (event.getStack().getItemMeta().getDisplayName().contains("Game E8") && event.getPlayer().hasPermission("tournament.g1team29to32"))
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Icey8");
-        else if (event.getStack().getItemMeta().getDisplayName().contains("Game F1") && event.getPlayer().hasPermission("tournament.g2team1to2"))
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Village");
-        if (event.getStack().getItemMeta().getDisplayName().contains("Game F2") && event.getPlayer().hasPermission("tournament.g2team3to4"))
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Village2");
-        else if (event.getStack().getItemMeta().getDisplayName().contains("FINALS") && event.getPlayer().hasPermission("tournament.game3"))
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "sudo " + event.getPlayer().getName() + " bw join Deserted");
-        else {
-            event.getPlayer().sendMessage("§c&LHEY!, §cSorry but you Havnt got Permission to do this Yet!");
-        }
     }
 }
